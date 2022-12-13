946240a432e00eb9cdff7cc5052a652d8f1ff4fe
Aaron Kable <aaronkable@gmail.com>
2022-12-13 13:24:11 +0800

    high priority

--- rerouter/views.py
+++ rerouter/views.py
@@ -12,6 +12,7 @@ def ingress(request, token, user):
         webhook = get_object_or_404(PingerIngressConfig, identifier=token)
 
         for r in webhook.routes.filter(active=True):
-            send_ping.delay(r.discord_webhook, request.body.decode())
+            send_ping.apply_async(
+                args=[r.discord_webhook, request.body.decode()], priority=1)
 
     return HttpResponse(status=204)
